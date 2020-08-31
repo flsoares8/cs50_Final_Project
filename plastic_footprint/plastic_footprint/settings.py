fdb7cd172d68317effb8f899cf91d62c79baa43e
Francisco Soares <franciscolsoares8@gmail.com>
2020-08-31 22:15:49 +0100

    Created the database connection amd the Customer model

--- plastic_footprint/plastic_footprint/settings.py
+++ plastic_footprint/plastic_footprint/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'plastic_footprint.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'plastic_tracker',
+        'USER': 'postgres',
+        'PASSWORD': 'plasticproject',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- plastic_footprint/plastic_tracker/models.py
+++ plastic_footprint/plastic_tracker/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    fullname = models.CharField(max_length=100)
+    monthly_balance = models.FloatField()
